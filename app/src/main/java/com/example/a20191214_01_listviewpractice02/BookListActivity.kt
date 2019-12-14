8db941aaeece8b00ac5a2eb0bae8962783ef9044
cho881020 <kj_cho@idealidea.co.kr>
2019-12-14 14:01:37 +0900

    책목록 뿌려주기까지 완성

--- app/src/main/java/com/example/a20191214_01_listviewpractice02/BookListActivity.kt
+++ app/src/main/java/com/example/a20191214_01_listviewpractice02/BookListActivity.kt
@@ -2,8 +2,16 @@ package com.example.a20191214_01_listviewpractice02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a20191214_01_listviewpractice02.adapters.BookAdapter
+import com.example.a20191214_01_listviewpractice02.adapters.GameAdapter
+import com.example.a20191214_01_listviewpractice02.datas.BookData
+import com.example.a20191214_01_listviewpractice02.datas.GameData
+import kotlinx.android.synthetic.main.activity_book_list.*
 
 class BookListActivity : BaseActivity() {
+
+    val bookList = ArrayList<BookData>()
+    var bookAdapter: BookAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +25,16 @@ class BookListActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        addBooks()
 
+        bookAdapter = BookAdapter(mContext, R.layout.book_list_item, bookList)
+        bookListView.adapter = bookAdapter
+    }
+
+    fun addBooks() {
+        bookList.add(BookData("드래곤볼", "일본"))
+        bookList.add(BookData("해리포터", "영국"))
+        bookList.add(BookData("반지의제왕", "미국"))
     }
 
 }
